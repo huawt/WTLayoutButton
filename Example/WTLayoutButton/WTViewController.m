e645959a1e498172a3deabfce3c078033b040ccc
huawentao <huawentao@vipkid.com.cn>
2019-11-04 18:25:36 +0800

    1.0.0

--- Example/WTLayoutButton/WTViewController.m
+++ Example/WTLayoutButton/WTViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "WTViewController.h"
+#import <WTLayoutButton/WTLayoutButton.h>
 
 @interface WTViewController ()
 
@@ -18,7 +19,40 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    CGFloat margin = 20;
+    CGFloat buttonWidth = (CGRectGetWidth(self.view.frame) - 20 * 3) / 2.0;
+    CGFloat buttonHeight = buttonWidth;
+    
+    [self addButton: [self generateButtonWithStyle:WTLayoutButtonStyleUpImageDownTitle]
+          withFrame:CGRectMake(margin, 64 + margin, buttonWidth, buttonHeight)];
+    
+    [self addButton: [self generateButtonWithStyle:WTLayoutButtonStyleUpTitleDownImage]
+          withFrame:CGRectMake(margin + buttonWidth + margin, 64 + margin, buttonWidth, buttonHeight)];
+    
+    [self addButton: [self generateButtonWithStyle:WTLayoutButtonStyleLeftImageRightTitle]
+          withFrame:CGRectMake(margin, 64 + margin + buttonHeight + margin, buttonWidth, buttonHeight)];
+    
+    [self addButton: [self generateButtonWithStyle:WTLayoutButtonStyleLeftTitleRightImage]
+          withFrame:CGRectMake(margin + buttonWidth + margin, 64 + margin + buttonHeight + margin, buttonWidth, buttonHeight)];
 }
+
+- (WTLayoutButton *)generateButtonWithStyle:(WTLayoutButtonStyle)style {
+    WTLayoutButton *button = [WTLayoutButton buttonWithType:UIButtonTypeCustom];
+    [button setImage:[UIImage imageNamed:@"Friend"] forState:UIControlStateNormal];
+    [button setTitle:@"Friend" forState:UIControlStateNormal];
+    [button setTitleColor:[UIColor darkTextColor] forState:UIControlStateNormal];
+    button.layoutStyle = style;
+    
+    button.layer.borderWidth = 1;
+    button.layer.borderColor = [UIColor magentaColor].CGColor;
+    return button;
+}
+
+- (void)addButton:(WTLayoutButton *)button withFrame:(CGRect)frame {
+    button.frame = frame;
+    [self.view addSubview:button];
+}
+
 
 - (void)didReceiveMemoryWarning
 {
